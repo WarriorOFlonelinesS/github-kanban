1dd0197eca7ade8b96d67e8a0e0afec2b263c026
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2024-02-29 20:52:23 +0200

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Board } from './components/Board';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Github Kanban';
+  }, []); 
   return (
     <>
       <Header />
